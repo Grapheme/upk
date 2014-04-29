7f197dcdae00a245a01b7171e7e60aefd13953ff
vkharseev <vladimir@realitygroup.ru>
2014-04-29 12:51:09 +0400

    Динамический проект

---
+++ www/app/views/templates/default/scripts.blade.php
@@ -0,0 +1,6 @@
+@if(Config::get('app.use_scripts_local'))
+	{{HTML::script('js/vendor/jquery.min.js');}}
+@else
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="{{asset('js/vendor/jquery.min.js');}}"><\/script>')</script>
+@endif
