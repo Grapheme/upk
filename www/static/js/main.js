fcb04747d15338baf48f3fb1ca73656676c309e3
Artem Tumin <arttumin@gmail.com>
2014-04-29 15:22:53 +0400

    +

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -1,6 +1,19 @@
+function positionitems() {
+  $items = $('.index-block');
+
+  var time = 200;
+
+  $items.each(function() {
+	  var that = $(this);
+      setTimeout( function(){ that.css({opacity: 1}); }, time);
+      time += 200;
+  });
+}
 $(function(){
 	$('.block-title').each(function(){
 		var pad = ( $('.index-block').width() - $(this).width() ) / 2;
 		$(this).css('padding-left', pad);
 	});
+
+	positionitems();
 });
